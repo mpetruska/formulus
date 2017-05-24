88877be919c14f1d11e7ebfdd69ea4efedfcda85
Mark Petruska <petruska.mark@gmail.com>
2017-05-24 19:42:31 +0200

    fixes identifier validation

--- src/Data/Identifier.purs
+++ src/Data/Identifier.purs
@@ -11,7 +11,7 @@ import Data.Array (many, singleton)
 import Data.Either (Either)
 import Data.String (fromCharArray)
 import Data.String.Regex (Regex, regex)
-import Data.String.Regex.Flags (ignoreCase)
+import Data.String.Regex.Flags (noFlags)
 import Text.Parsing.Parser.String (char)
 import Text.Parsing.Parser.Token (digit)
 
@@ -35,7 +35,7 @@ identifier x =
     matches' (toValidated' identifierRegex) "identifier format is not valid" x
 
 identifierRegex :: Either String Regex
-identifierRegex = regex "^[\\_a-z][\\_a-z0-9]*$" ignoreCase
+identifierRegex = regex "^[\\_a-z][\\_a-z0-9]*$" noFlags
 
 identifierParser :: StringParser Identifier
 identifierParser = do
